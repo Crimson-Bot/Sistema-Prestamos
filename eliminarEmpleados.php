8de5eeb20ba6689273905dee1f62cf9f311068b8
Crimson <crimsonfm69@gmail.com>
2021-11-06 18:10:18 -0600

    verificación de eliminar usuario

--- eliminarEmpleados.php
+++ eliminarEmpleados.php
@@ -1,27 +1,116 @@
 <?php
 $idd = $_GET['idEliminado'];
- 
+global $claveId, $prestamoId;
+$claveId = "";
+$prestamoId = "";
+
+// -----------------------------------------------------------------------------------------------------------
 $leer = fopen("credenciales.txt", "r");
-$escribir = fopen("temp.txt", "a+");
 while (!feof($leer)) {
-    $id = fgets($leer);
-    $nombre = fgets($leer);
-    $tipo = fgets($leer);
-    $sexo = fgets($leer);
-    $foto = fgets($leer);
-    $salario = fgets($leer);
-    if ($idd != $id) {
-        fputs($escribir, $id);
-        fputs($escribir, $nombre);
-        fputs($escribir, $tipo);
-        fputs($escribir, $sexo);
-        fputs($escribir, $foto);
-        fputs($escribir, $salario);
+    $claveId = fgets($leer);
+    if ($idd == $claveId) {
+        $leerP = fopen("prestamos.txt", "r");
+        while (!feof($leerP)) {
+            $prestamoId = fgets($leerP);
+            if ($idd == $prestamoId) {
+                echo "<script>alert('El usuario tiene un prestamo pendiente!!!');</script>";
+                echo "<META HTTP-EQUIV='Refresh' CONTENT='0; url=dasboardAdmin.php'>";
+                break;
+            } else {
+                $leer = fopen("credenciales.txt", "r");
+                $escribir = fopen("temp.txt", "a+");
+                while (!feof($leer)) {
+                    $id = fgets($leer);
+                    $nombre = fgets($leer);
+                    $tipo = fgets($leer);
+                    $sexo = fgets($leer);
+                    $foto = fgets($leer);
+                    $salario = fgets($leer);
+                    if ($idd != $id) {
+                        fputs($escribir, $id);
+                        fputs($escribir, $nombre);
+                        fputs($escribir, $tipo);
+                        fputs($escribir, $sexo);
+                        fputs($escribir, $foto);
+                        fputs($escribir, $salario);
+                    }
+                }
+                fclose($leer);
+                fclose($escribir);
+                if (rename("temp.txt", "credenciales.txt")) {
+                    echo "<script>alert('Datos Eliminados Exitosamente!!!');</script>";
+                    echo "<META HTTP-EQUIV='Refresh' CONTENT='0; url=dasboardAdmin.php'>";
+                    break;
+                }
+            }
+        }
+        break;
+    } else {
+        echo "<script>alert('El usuario que ingreso no existe!!!');</script>";
+        echo "<META HTTP-EQUIV='Refresh' CONTENT='0; url=dasboardAdmin.php'>";
+        break;
     }
 }
-fclose($leer);
-fclose($escribir);
-if (rename("temp.txt", "credenciales.txt")) {
-    echo "<script>alert('Datos Eliminados Exitosamente!!!');</script>";
-    echo "<META HTTP-EQUIV='Refresh' CONTENT='0; url=dasboardAdmin.php'>";
-}
+// -----------------------------------------------------------------------------------------------------------
+// $leerP = fopen("prestamos.txt", "r");
+// while (!feof($leerP)) {
+//     $prestamoId = fgets($leerP);
+//     if ($idd == $prestamoId) {
+//         echo "<script>alert('El usuario tiene un prestamo pendiente!!!');</script>";
+//         echo "<META HTTP-EQUIV='Refresh' CONTENT='0; url=dasboardAdmin.php'>";
+//         break;
+//     } else {
+//         $leer = fopen("credenciales.txt", "r");
+//         $escribir = fopen("temp.txt", "a+");
+//         while (!feof($leer)) {
+//             $id = fgets($leer);
+//             $nombre = fgets($leer);
+//             $tipo = fgets($leer);
+//             $sexo = fgets($leer);
+//             $foto = fgets($leer);
+//             $salario = fgets($leer);
+//             if ($idd != $id) {
+//                 fputs($escribir, $id);
+//                 fputs($escribir, $nombre);
+//                 fputs($escribir, $tipo);
+//                 fputs($escribir, $sexo);
+//                 fputs($escribir, $foto);
+//                 fputs($escribir, $salario);
+//             }
+//         }
+//         fclose($leer);
+//         fclose($escribir);
+//         if (rename("temp.txt", "credenciales.txt")) {
+//             echo "<script>alert('Datos Eliminados Exitosamente!!!');</script>";
+//             echo "<META HTTP-EQUIV='Refresh' CONTENT='0; url=dasboardAdmin.php'>";
+//             break;
+//         }
+//     }
+// }
+
+// ------------------------------------------------------------------------------------------------------------
+
+// $leer = fopen("credenciales.txt", "r");
+// $escribir = fopen("temp.txt", "a+");
+// while (!feof($leer)) {
+//     $id = fgets($leer);
+//     $nombre = fgets($leer);
+//     $tipo = fgets($leer);
+//     $sexo = fgets($leer);
+//     $foto = fgets($leer);
+//     $salario = fgets($leer);
+//     if ($idd != $id) {
+//         fputs($escribir, $id);
+//         fputs($escribir, $nombre);
+//         fputs($escribir, $tipo);
+//         fputs($escribir, $sexo);
+//         fputs($escribir, $foto);
+//         fputs($escribir, $salario);
+//     }
+// }
+// fclose($leer);
+// fclose($escribir);
+// if (rename("temp.txt", "credenciales.txt")) {
+//     echo "<script>alert('Datos Eliminados Exitosamente!!!');</script>";
+//     echo "<META HTTP-EQUIV='Refresh' CONTENT='0; url=dasboardAdmin.php'>";
+// }
